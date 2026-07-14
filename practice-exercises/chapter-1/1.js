6dadb14c3d6be17c9913eeb3429a64467acde164
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-07-14 22:36:05 +0600

    first commit

---
+++ practice-exercises/chapter-1/1.js
@@ -0,0 +1,5 @@
+//1. Print your name, your department, and your university on three separate lines.
+
+console.log("Sadia Rahman");
+console.log("Department of Computer Science & Engineering");
+console.log("Metropolitan University");
