07f9b63c0a712e8ef709945d638089fcbd19a962
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-07-14 23:18:52 +0600

    fixed chapter-1 exercise-1 file

--- practice-exercises/chapter-1/exercise-1.js
+++ practice-exercises/chapter-1/exercise-1.js
@@ -1,4 +1,4 @@
-//1. Print your name, your department, and your university on three separate lines.
+//1. Print your name, your department, and your university on three separate lines..
 
 console.log("Sadia Rahman");
 console.log("Department of Computer Science & Engineering");
